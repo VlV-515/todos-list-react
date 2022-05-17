13b1482ceb001e9a251f2e5504b5cc8d5282e20a
VlV <92279938+VlV-515@users.noreply.github.com>
2022-05-17 00:49:21 -0500

    [X]Limpieza inicial [X]Feat⚙️Crea el componente <Logo>

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import logo from '../../logo.svg'
+import './logo.css'
+
+function Logo() {
+  return (
+    <div className='logo-contenedor'>
+      <img src={logo} className='logo' />
+    </div>
+  )
+}
+export default Logo
